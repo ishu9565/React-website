05b20b74aee8f7f05cd89863307e6cff08ca88df
ishu9565 <ishuguptajnv@gmail.com>
2022-06-23 00:58:36 +0530

    Final commit

--- src/Home.js
+++ src/Home.js
@@ -2,10 +2,10 @@ import React,{Component} from 'react';
 
 export class Home extends Component{
     render(){
-        return (
+        return(
             <div>
-            <h3>This is Home page</h3>
-           </div>
+                <h3>This is Home page</h3>
+            </div>
         )
     }
 }
